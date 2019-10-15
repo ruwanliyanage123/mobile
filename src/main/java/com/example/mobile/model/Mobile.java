2fc431ea8bb35fc73777e1f077377560997aadac
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-10-15 09:29:43 +0530

    Add model

--- src/main/java/com/example/mobile/model/Mobile.java
+++ src/main/java/com/example/mobile/model/Mobile.java
@@ -1,4 +1,37 @@
 package com.example.mobile.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "phones")
 public class Mobile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int serialNumber;
+    private String modelName;
+
+    public Mobile(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
 }
